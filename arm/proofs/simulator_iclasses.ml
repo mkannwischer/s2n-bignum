f9ebd40af24087c503ecaca008be22edd166afab
Matthias J. Kannwischer <matthias@kannwischer.eu>
2026-02-23 17:07:14 +0800

    Arm64: Add vector CMGT
    Add signed greater-than compare (CMGT) for Advanced SIMD three-same
    register form, analogous to the existing CMGE (signed >=) and CMHI
    (unsigned >). Uses ival x > ival y for signed comparison.
    
    Needed for mldsa-native poly_decompose.
    Signed-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>

--- arm/proofs/simulator_iclasses.ml
+++ arm/proofs/simulator_iclasses.ml
@@ -132,6 +132,9 @@ let iclasses =
   (*** CMGE, vector ***)
   "0x001110xx1xxxxx001111xxxxxxxxxx";
 
+  (*** CMGT, vector ***)
+  "0x001110xx1xxxxx001101xxxxxxxxxx";
+
   (*** CNT, bias to defined size = 0 ***)
   "0x00111000100000010110xxxxxxxxxx";
   "0x001110xx100000010110xxxxxxxxxx";
